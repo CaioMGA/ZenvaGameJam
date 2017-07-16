516e3361ae6de190774dbbb09e065b4795a0abbb
Caio Marchi <caiomga@gmail.com>
2017-07-16 17:34:41 -0300

    Bullet Hero loading assets

--- js/load.js
+++ js/load.js
@@ -1,10 +1,10 @@
 var loadState = {
 	preload:function (){
-		game.add.text(80, 80, "Loading . . .");
+		var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill:'#ffffff'});
 
-		game.load.image("box", "img/box.png");
 		game.load.spritesheet("hero_spritesheet", "img/hero_spritesheet.png", 32, 32);
 		game.load.spritesheet("targetIndicator", "img/cursorIcons.png", 32, 32);
+		game.load.spritesheet("enemy_bullet", "img/enemy_bullet.png", 46, 32);
 
 		game.load.tilemap("map", "misc/map1.json", null, Phaser.Tilemap.TILED_JSON);
 		game.load.image("tiles", "img/tiles_purple.png");
