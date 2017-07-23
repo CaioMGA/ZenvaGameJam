f76af6f60fa4a884f97e2cbd07c8d1260ca5a0b9
Caio Marchi <caiomga@gmail.com>
2017-07-23 05:32:11 -0300

    Door particles implemented

--- js/door.js
+++ js/door.js
@@ -2,6 +2,7 @@ function createDoor(_x, _y){
 	return {
 		"sprite": null,
 		"opened": false,
+		"emitter" : null,
 		"createAnimations": function(){
 			this.sprite = game.add.sprite(_x, _y, 'door', 0);
 			this.sprite.animations.add('closed', [0], 8, false);
@@ -12,11 +13,27 @@ function createDoor(_x, _y){
 			this.sprite.body.setSize(40, 45, -5, -5);
 		},
 		"open" : function(){
-			this.sprite.animations.play("opened");
-			this.opened = true;
+			if(!this.opened){
+				this.sprite.animations.play("opened");
+				this.opened = true;
+				this.emitter.start(true, 1000, null, 10);
+			}
 		},
 		"init" : function(){
 			this.createAnimations();
+			this.emitter = game.add.emitter(
+				this.sprite.x + this.sprite.width / 2,
+				this.sprite.y + this.sprite.height / 2,
+				10);
+			this.emitter.makeParticles("particles");
+			this.emitter.minParticleScale = 0.2;
+			this.emitter.maxParticleScale = 0.4;
+			this.emitter.gravity = 100;
+			this.emitter.setAlpha(1, 0, 1000, Phaser.Easing.Linear.None, false);
+			this.emitter.autoAlpha = true;
+			this.emitter.forEach( function(particle) {
+				particle.tint = 0xff2222;
+			});
 		}
 	}
 }
--- js/load.js
+++ js/load.js
@@ -12,6 +12,7 @@ var loadState = {
 		game.load.spritesheet("button", "img/small_button.png", 32, 32);
 		game.load.spritesheet("menu_button", "img/big_button.png", 276, 46);
 		game.load.spritesheet("icons", "img/icons.png", 32, 32);
+		game.load.spritesheet("particles", "img/particles.png", 64, 64);
 
 		for(i = 1; i <= 10; i ++){
 			game.load.tilemap("level"+i.toString(), "misc/1-" + i.toString()+".json", null, Phaser.Tilemap.TILED_JSON);
