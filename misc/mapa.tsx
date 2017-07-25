b7d7f6cf82fd3a1e81b614742874eefc4b558fab
Caio Marchi <caiomga@gmail.com>
2017-07-25 00:17:27 -0300

    gitignore fixed. map 1-1.json added to te project

---
+++ misc/mapa.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Kenney Rogue-like" tilewidth="16" tileheight="16" tilecount="1920" columns="60">
+ <grid orientation="orthogonal" width="17" height="17"/>
+ <image source="../../../../../Users/Caio/Pictures/game assets/Kenney game Assets/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+</tileset>
---
+++ misc/purplewalls.tsx
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Purple walls" tilewidth="16" tileheight="16" tilecount="36" columns="9">
+ <image source="../img/tiles_purple.png" width="144" height="64"/>
+ <tile id="0" type="wall"/>
+ <tile id="1" type="wall"/>
+ <tile id="2" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0.0909091" y="-0.0909091" width="15.8182" height="15.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" type="wall"/>
+ <tile id="6" type="wall"/>
+ <tile id="7" type="goal"/>
+ <tile id="8" type="goal"/>
+ <tile id="9" type="wall"/>
+ <tile id="10" type="wall"/>
+ <tile id="11" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="floor" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="14" type="wall"/>
+ <tile id="15" type="wall"/>
+ <tile id="16" type="goal"/>
+ <tile id="17" type="goal"/>
+ <tile id="18" type="floor">
+  <objectgroup draworder="index">
+   <object id="1" type="floor" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="20" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="21" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="22" type="wall">
+  <objectgroup draworder="index">
+   <object id="1" type="wall" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
